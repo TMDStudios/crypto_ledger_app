7bc4d491492ac811b919bdefbd1c192fa67c057d
TMD Studios <tmdstudioscontact@gmail.com>
2021-05-02 22:25:27 +0400

    General Practice

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, Platform, SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Bob");
+  const [person, setPerson] = useState({ name: "Fred", age: 25 });
+  const clickHandler = () => {
+    setPerson({ name: "Tim", age: 42 });
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.header_text}>Crypto Ledger App</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>This is {name}</Text>
+        <Text>
+          His name is {person.name} and he is {person.age}
+        </Text>
+        <Text style={styles.header_text}>Enter name:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Type your name"
+          onChangeText={(val) => setName(val)}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button onPress={clickHandler} title="Update" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  header: {
+    backgroundColor: "blue",
+    padding: 20,
+    height: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header_text: {
+    fontWeight: "bold",
+    color: "white",
+  },
+  body: {
+    backgroundColor: "gold",
+    padding: 20,
+    flex: 1,
+  },
+  buttonContainer: {
+    backgroundColor: "blue",
+    padding: 20,
+  },
+  input: {
+    backgroundColor: "silver",
+    color: "white",
+    borderWidth: 2,
+    borderColor: "#777",
+    paddingLeft: 8,
   },
 });
