227139a94b8c1ee8a58c2a67b96ec6b9352d9cd9
TMD Studios <tmdstudioscontact@gmail.com>
2021-05-03 14:01:10 +0400

    Added ScrollView

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { Button, Platform, SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  StatusBar,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
   const [name, setName] = useState("Bob");
@@ -8,6 +17,78 @@ export default function App() {
   const clickHandler = () => {
     setPerson({ name: "Tim", age: 42 });
   };
+  const [coin, setCoin] = useState([
+    {
+      symbol: "btc",
+      name: "Bitcoin",
+      price: 56000,
+      price1h: 10,
+      price24h: 89,
+      priceBTC: 1,
+      priceETH: 20000,
+      key: "1",
+    },
+    {
+      symbol: "eth",
+      name: "Ethereum",
+      price: 2800,
+      price1h: 6,
+      price24h: 214,
+      priceBTC: 0.002,
+      priceETH: 1,
+      key: "2",
+    },
+    {
+      symbol: "xmr",
+      name: "Monero",
+      price: 420,
+      price1h: 15,
+      price24h: 71,
+      priceBTC: 0.0001,
+      priceETH: 0.01,
+      key: "3",
+    },
+    {
+      symbol: "xmr",
+      name: "Monero4",
+      price: 420,
+      price1h: 15,
+      price24h: 71,
+      priceBTC: 0.0001,
+      priceETH: 0.01,
+      key: "4",
+    },
+    {
+      symbol: "xmr",
+      name: "Monero5",
+      price: 420,
+      price1h: 15,
+      price24h: 71,
+      priceBTC: 0.0001,
+      priceETH: 0.01,
+      key: "5",
+    },
+    {
+      symbol: "xmr",
+      name: "Monero6",
+      price: 420,
+      price1h: 15,
+      price24h: 71,
+      priceBTC: 0.0001,
+      priceETH: 0.01,
+      key: "6",
+    },
+    {
+      symbol: "xmr",
+      name: "Monero7",
+      price: 420,
+      price1h: 15,
+      price24h: 71,
+      priceBTC: 0.0001,
+      priceETH: 0.01,
+      key: "7",
+    },
+  ]);
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -24,6 +105,24 @@ export default function App() {
           placeholder="Type your name"
           onChangeText={(val) => setName(val)}
         />
+        <Text>The status bar is: {StatusBar.currentHeight}</Text>
+        <ScrollView>
+          {coin.map((item) => {
+            return (
+              <View key={item.key}>
+                <Text style={styles.list_head}>
+                  {item.name} - {item.price}
+                </Text>
+                <Text style={styles.list_body}>
+                  {item.price1h} - {item.price24h}
+                </Text>
+                <Text style={styles.list_body}>
+                  {item.priceBTC} - {item.priceETH}
+                </Text>
+              </View>
+            );
+          })}
+        </ScrollView>
       </View>
       <View style={styles.buttonContainer}>
         <Button onPress={clickHandler} title="Update" />
@@ -64,5 +163,13 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "#777",
     paddingLeft: 8,
+  },
+  list_head: {
+    paddingTop: 50,
+    color: "blue",
+    fontWeight: "bold",
+  },
+  list_body: {
+    color: "white",
   },
 });
